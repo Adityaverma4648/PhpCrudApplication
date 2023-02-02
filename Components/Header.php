26f2788881dd782319e822a63f62fed7974982bb
Aditya verma <87409406+Adityaverma4648@users.noreply.github.com>
2023-02-02 18:27:38 +0530

    navbar Finished

---
+++ Components/Header.php
@@ -0,0 +1,29 @@
+<!-- <link rel="stylesheet" href="../styles/style.css"> -->
+<header class="header The-Black-and-Red-5-hex" id="header">
+    <!--  navbar  -->
+    <nav>
+        <div class="logoContainer">
+            LOGO
+        </div>
+        <ul>
+            <li>
+                <a href="./../index.php">
+                    Home
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    About
+                </a>
+            </li>
+        </ul>
+        <div>
+            <a href="./Login.php">Login</a>
+            <a href="./Register.php">Register</a>
+        </div>
+        <div class="toggleIconContainer">
+            B
+        </div>
+    </nav>
+    <!--  navbar ends -->
+</header>
---
+++ Register.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Register</title>
+    <!--  google fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Cookie&family=Poppins:wght@500;600&family=Raleway&family=Roboto&display=swap" rel="stylesheet">
+    <!--  fonts imports ends here -->
+
+    <link rel="stylesheet" href="./styles/style.css">
+</head>
+
+<body>
+    <?php include "./Components/Header.php" ?>
+    <div>
+        Register
+    </div>
+</body>
+
+</html>

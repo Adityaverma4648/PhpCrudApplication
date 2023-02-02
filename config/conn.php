0923f39083a19bc22d5b71dcf905c59380e4f21a
Aditya verma <87409406+Adityaverma4648@users.noreply.github.com>
2023-02-02 16:41:12 +0530

    header initialized

--- config/conn.php
+++ config/conn.php
@@ -2,14 +2,13 @@
 $serverName = "localhost";
 $userName = "root";
 $password = "";
+$db = "PhpCrudApplication";
 
 //   create connection
-$conn = new mysqli($serverName, $userName, $password);
+$conn = new mysqli($serverName, $userName, $password, $db);
 
 
 //  check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
-} else {
-    echo "Connection established";
 }
---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+include "./config/conn.php";
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Home</title>
+</head>
+
+<body>
+  <!--  header  -->
+  <?php include "./Pages/Header.php"; ?>
+  <!--  header ends -->
+</body>
+
+</html>
